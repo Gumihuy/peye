c262f6eaeb26fb9c65fe5ecea21a34f73b83f4c0
Gumihuy <116326458+Gumihuy@users.noreply.github.com>
2025-11-03 20:35:47 +0700

    Add files via upload

--- topologi.py
+++ topologi.py
@@ -9,6 +9,7 @@ from mininet.log import setLogLevel, info
 
 
 class LinuxRouter(Node):
+    "Router dengan IP Forwarding"
     def config(self, **params):
         super(LinuxRouter, self).config(**params)
         self.cmd('sysctl -w net.ipv4.ip_forward=1')
@@ -39,6 +40,7 @@ class MyTopo(Topo):
         self.addLink(r1, r2,
                      intfName1='r1-eth1', params1={'ip': '10.0.1.1/30'},
                      intfName2='r2-eth0', params2={'ip': '10.0.1.2/30'})
+
         self.addLink(r2, r3,
                      intfName1='r2-eth2', params1={'ip': '10.0.3.1/30'},
                      intfName2='r3-eth0', params2={'ip': '10.0.3.2/30'})
@@ -52,21 +54,32 @@ def run():
     info('\n*** Setting Routing Antar Router ***\n')
     r1, r2, r3 = net['r1'], net['r2'], net['r3']
 
-    # Routing antar router
+    # Routing di R1
     r1.cmd('ip route add 10.0.2.0/24 via 10.0.1.2')
     r1.cmd('ip route add 10.0.3.0/30 via 10.0.1.2')
     r1.cmd('ip route add 10.0.4.0/24 via 10.0.1.2')
 
+    # Routing di R2
     r2.cmd('ip route add 10.0.0.0/24 via 10.0.1.1')
     r2.cmd('ip route add 10.0.4.0/24 via 10.0.3.2')
 
+    # Routing di R3
     r3.cmd('ip route add 10.0.0.0/24 via 10.0.3.1')
     r3.cmd('ip route add 10.0.2.0/24 via 10.0.3.1')
+
+    # Tambahan untuk stabilitas link antar router
+    r1.cmd('arp -s 10.0.1.2 00:00:00:00:01:02')
+    r2.cmd('arp -s 10.0.1.1 00:00:00:00:01:01')
+    r2.cmd('arp -s 10.0.3.2 00:00:00:00:03:02')
+    r3.cmd('arp -s 10.0.3.1 00:00:00:00:03:01')
 
     info('\n*** Tes koneksi dari h1 ke h2 dan h3 ***\n')
     h1 = net['h1']
     h1.cmdPrint('ping -c 2 10.0.2.2')
     h1.cmdPrint('ping -c 2 10.0.4.2')
+
+    info('\n*** Uji konektivitas keseluruhan (pingall) ***\n')
+    net.pingAll()
 
     CLI(net)
     net.stop()
