4d542fd0dc7f73656bdecb7c4bb06d0b424ff9b4
Gumihuy <116326458+Gumihuy@users.noreply.github.com>
2025-11-03 22:27:05 +0700

    Add files via upload

--- topologi.py
+++ topologi.py
@@ -8,6 +8,7 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from time import sleep
 
+
 class LinuxRouter(Node):
     def config(self, **params):
         super(LinuxRouter, self).config(**params)
@@ -16,6 +17,7 @@ class LinuxRouter(Node):
     def terminate(self):
         self.cmd('sysctl -w net.ipv4.ip_forward=0')
         super(LinuxRouter, self).terminate()
+
 
 class MyTopo(Topo):
     def build(self):
@@ -29,12 +31,12 @@ class MyTopo(Topo):
         h2 = self.addHost('h2', ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
         h3 = self.addHost('h3', ip='10.0.4.2/24', defaultRoute='via 10.0.4.1')
 
-        # Koneksi host-router
+        # Host ke router
         self.addLink(h1, r1, intfName2='r1-eth0', params2={'ip': '10.0.0.1/24'})
         self.addLink(h2, r2, intfName2='r2-eth1', params2={'ip': '10.0.2.1/24'})
         self.addLink(h3, r3, intfName2='r3-eth1', params2={'ip': '10.0.4.1/24'})
 
-        # Koneksi antar router
+        # Router antar-router
         self.addLink(r1, r2,
                      intfName1='r1-eth1', params1={'ip': '10.0.1.1/30'},
                      intfName2='r2-eth0', params2={'ip': '10.0.1.2/30'})
@@ -43,6 +45,7 @@ class MyTopo(Topo):
                      intfName1='r2-eth2', params1={'ip': '10.0.3.1/30'},
                      intfName2='r3-eth0', params2={'ip': '10.0.3.2/30'})
 
+
 def run():
     topo = MyTopo()
     net = Mininet(topo=topo, link=TCLink, controller=None)
@@ -50,48 +53,49 @@ def run():
 
     r1, r2, r3 = net['r1'], net['r2'], net['r3']
 
-    info('\n*** Setting Routing Antar Router ***\n')
+    info("\n*** Setting Routing Antar Router ***\n")
 
-    # Routing antar router
+    # R1 routes
+    r1.cmd('ip route add 10.0.1.0/30 dev r1-eth1')
     r1.cmd('ip route add 10.0.2.0/24 via 10.0.1.2')
     r1.cmd('ip route add 10.0.3.0/30 via 10.0.1.2')
     r1.cmd('ip route add 10.0.4.0/24 via 10.0.1.2')
 
+    # R2 routes
     r2.cmd('ip route add 10.0.0.0/24 via 10.0.1.1')
     r2.cmd('ip route add 10.0.4.0/24 via 10.0.3.2')
+    r2.cmd('ip route add 10.0.1.0/30 dev r2-eth0')
+    r2.cmd('ip route add 10.0.3.0/30 dev r2-eth2')
 
+    # Tambahkan route balik penting!
+    r2.cmd('ip route add 10.0.0.0/24 via 10.0.1.1')
+    r2.cmd('ip route add 10.0.2.0/24 dev r2-eth1')
+
+    # R3 routes
+    r3.cmd('ip route add 10.0.3.0/30 dev r3-eth0')
     r3.cmd('ip route add 10.0.0.0/24 via 10.0.3.1')
+    r3.cmd('ip route add 10.0.1.0/30 via 10.0.3.1')
     r3.cmd('ip route add 10.0.2.0/24 via 10.0.3.1')
-
-    # Tambahan untuk memastikan koneksi antar-router selalu hidup
-    for r in (r1, r2, r3):
-        r.cmd('ip route add 10.0.1.0/30 dev r1-eth1 || true')
-        r.cmd('ip route add 10.0.3.0/30 dev r3-eth0 || true')
-        r.cmd('ip neigh flush all')
 
     sleep(2)
 
-    info('\n*** Pre-warming koneksi antar-router (ping 1x biar ARP aktif) ***\n')
+    info("\n*** Ping antar-router dulu (aktivasi ARP) ***\n")
     r1.cmd('ping -c1 10.0.1.2')
     r2.cmd('ping -c1 10.0.3.2')
     r3.cmd('ping -c1 10.0.3.1')
     sleep(2)
 
-    # Flush ARP sekali lagi setelah koneksi aktif
-    for r in (r1, r2, r3):
-        r.cmd('ip neigh flush all')
+    info("\n*** Tes koneksi dari R1 ke R3 via R2 ***\n")
+    r1.cmdPrint('ping -c2 10.0.3.2')
+    r1.cmdPrint('ping -c2 10.0.4.1')
 
-    sleep(2)
-    info('\n*** Tes koneksi dari h1 ke h2 dan h3 ***\n')
-    h1 = net['h1']
-    h1.cmdPrint('ping -c 3 10.0.2.2')
-    h1.cmdPrint('ping -c 3 10.0.4.2')
-
-    info('\n*** Tes pingall keseluruhan ***\n')
+    sleep(1)
+    info("\n*** Ping all host ***\n")
     net.pingAll()
 
     CLI(net)
     net.stop()
+
 
 if __name__ == '__main__':
     setLogLevel('info')
