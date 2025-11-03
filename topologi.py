d8acace9c77c2c0e34606c9c58d9b198163ebca0
Gumihuy <116326458+Gumihuy@users.noreply.github.com>
2025-11-03 21:48:30 +0700

    Add files via upload

--- topologi.py
+++ topologi.py
@@ -3,14 +3,14 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Node
-from mininet.cli import CLI
 from mininet.link import TCLink
+from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from time import sleep
 
 
 class LinuxRouter(Node):
-    "Router dengan IP Forwarding aktif"
+    "Router dengan IP forwarding"
     def config(self, **params):
         super(LinuxRouter, self).config(**params)
         self.cmd('sysctl -w net.ipv4.ip_forward=1')
@@ -22,29 +22,29 @@ class LinuxRouter(Node):
 
 class MyTopo(Topo):
     def build(self):
-        # Tambah router sesuai gambar
+        # Router
         r1 = self.addNode('r1', cls=LinuxRouter, ip='10.0.0.1/24')
-        r2 = self.addNode('r2', cls=LinuxRouter, ip='10.0.1.2/24')
-        r3 = self.addNode('r3', cls=LinuxRouter, ip='10.0.3.2/24')
+        r2 = self.addNode('r2', cls=LinuxRouter, ip='10.0.2.1/24')
+        r3 = self.addNode('r3', cls=LinuxRouter, ip='10.0.4.1/24')
 
-        # Tambah host
+        # Host
         h1 = self.addHost('h1', ip='10.0.0.2/24', defaultRoute='via 10.0.0.1')
         h2 = self.addHost('h2', ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
         h3 = self.addHost('h3', ip='10.0.4.2/24', defaultRoute='via 10.0.4.1')
 
-        # Hubungkan host ke router
+        # Koneksi host-router
         self.addLink(h1, r1, intfName2='r1-eth0', params2={'ip': '10.0.0.1/24'})
         self.addLink(h2, r2, intfName2='r2-eth1', params2={'ip': '10.0.2.1/24'})
         self.addLink(h3, r3, intfName2='r3-eth1', params2={'ip': '10.0.4.1/24'})
 
-        # Hubungan antar-router sesuai topologi
+        # Koneksi antar router
         self.addLink(r1, r2,
-                     intfName1='r1-eth1', params1={'ip': '10.0.1.1/24'},
-                     intfName2='r2-eth0', params2={'ip': '10.0.1.2/24'})
+                     intfName1='r1-eth1', params1={'ip': '10.0.1.1/30'},
+                     intfName2='r2-eth0', params2={'ip': '10.0.1.2/30'})
 
         self.addLink(r2, r3,
-                     intfName1='r2-eth2', params1={'ip': '10.0.3.1/24'},
-                     intfName2='r3-eth0', params2={'ip': '10.0.3.2/24'})
+                     intfName1='r2-eth2', params1={'ip': '10.0.3.1/30'},
+                     intfName2='r3-eth0', params2={'ip': '10.0.3.2/30'})
 
 
 def run():
@@ -52,27 +52,26 @@ def run():
     net = Mininet(topo=topo, link=TCLink, controller=None)
     net.start()
 
+    info('\n*** Menambahkan routing statis antar router ***\n')
     r1, r2, r3 = net['r1'], net['r2'], net['r3']
 
-    info('\n*** Menambahkan routing statis antar router ***\n')
-
-    # R1 routing
+    # Routing R1
     r1.cmd('ip route add 10.0.2.0/24 via 10.0.1.2')
-    r1.cmd('ip route add 10.0.3.0/24 via 10.0.1.2')
+    r1.cmd('ip route add 10.0.3.0/30 via 10.0.1.2')
     r1.cmd('ip route add 10.0.4.0/24 via 10.0.1.2')
 
-    # R2 routing
+    # Routing R2
     r2.cmd('ip route add 10.0.0.0/24 via 10.0.1.1')
     r2.cmd('ip route add 10.0.4.0/24 via 10.0.3.2')
 
-    # R3 routing
+    # Routing R3
     r3.cmd('ip route add 10.0.0.0/24 via 10.0.3.1')
     r3.cmd('ip route add 10.0.2.0/24 via 10.0.3.1')
 
     info('\n*** Menunggu routing stabil... ***\n')
-    sleep(2)
+    sleep(3)
 
-    info('\n*** Uji konektivitas host ***\n')
+    info('\n*** Uji konektivitas seluruh host ***\n')
     net.pingAll()
 
     CLI(net)
